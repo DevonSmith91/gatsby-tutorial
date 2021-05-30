1b505811be21f901e4406bd89a8c1feaf2883388
Devon <devonsmith91@gmail.com>
2021-05-29 22:27:11 -0400

    finished part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>I like to blow glass, make art, and play video games.</p>
+		</Layout>
+  );
+};
+
+export default AboutPage;
